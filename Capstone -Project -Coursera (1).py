02eda3f86c495b774bec0a875869de27b8c8db2a
shubh2579 <73743880+shubh2579@users.noreply.github.com>
2021-04-06 01:47:10 +0530

    Add files via upload

---
+++ Capstone -Project -Coursera (1).py
@@ -0,0 +1,443 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <a href="https://www.bigdatauniversity.com"><img src="https://ibm.box.com/shared/static/cw2c7r3o20w9zn8gkecaeyjhgw3xdgbj.png" width="400" align="center"></a>
+# 
+# <h1 align="center"><font size="5">Classification with Python</font></h1>
+
+# In this notebook we try to practice all the classification algorithms that we learned in this course.
+# 
+# We load a dataset using Pandas library, and apply the following algorithms, and find the best one for this specific dataset by accuracy evaluation methods.
+# 
+# Lets first load required libraries:
+
+# In[2]:
+
+
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import NullFormatter
+import pandas as pd
+import numpy as np
+import matplotlib.ticker as ticker
+from sklearn import preprocessing
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# ### About dataset
+
+# This dataset is about past loans. The __Loan_train.csv__ data set includes details of 346 customers whose loan are already paid off or defaulted. It includes following fields:
+# 
+# | Field          | Description                                                                           |
+# |----------------|---------------------------------------------------------------------------------------|
+# | Loan_status    | Whether a loan is paid off on in collection                                           |
+# | Principal      | Basic principal loan amount at the                                                    |
+# | Terms          | Origination terms which can be weekly (7 days), biweekly, and monthly payoff schedule |
+# | Effective_date | When the loan got originated and took effects                                         |
+# | Due_date       | Since it’s one-time payoff schedule, each loan has one single due date                |
+# | Age            | Age of applicant                                                                      |
+# | Education      | Education of applicant                                                                |
+# | Gender         | The gender of applicant                                                               |
+
+# Lets download the dataset
+
+# In[3]:
+
+
+get_ipython().system('wget -O loan_train.csv https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/loan_train.csv')
+
+
+# ### Load Data From CSV File  
+
+# In[4]:
+
+
+df = pd.read_csv('loan_train.csv')
+df.head()
+
+
+# In[5]:
+
+
+df.shape
+
+
+# ### Convert to date time object 
+
+# In[6]:
+
+
+df['due_date'] = pd.to_datetime(df['due_date'])
+df['effective_date'] = pd.to_datetime(df['effective_date'])
+df.head()
+
+
+# # Data visualization and pre-processing
+# 
+# 
+
+# Let’s see how many of each class is in our data set 
+
+# In[7]:
+
+
+df['loan_status'].value_counts()
+
+
+# 260 people have paid off the loan on time while 86 have gone into collection 
+# 
+
+# Lets plot some columns to underestand data better:
+
+# In[8]:
+
+
+# notice: installing seaborn might takes a few minutes
+get_ipython().system('conda install -c anaconda seaborn -y')
+
+
+# In[8]:
+
+
+import seaborn as sns
+
+bins = np.linspace(df.Principal.min(), df.Principal.max(), 10)
+g = sns.FacetGrid(df, col="Gender", hue="loan_status", palette="Set1", col_wrap=2)
+
+g.map(plt.hist, 'Principal', bins=bins, ec="k")
+
+g.axes[-1].legend()
+
+
+# In[9]:
+
+
+bins = np.linspace(df.age.min(), df.age.max(), 10)
+g = sns.FacetGrid(df, col="Gender", hue="loan_status", palette="Set1", col_wrap=2)
+g.map(plt.hist, 'age', bins=bins, ec="k")
+
+g.axes[-1].legend()
+plt.show()
+
+
+# # Pre-processing:  Feature selection/extraction
+
+# ### Lets look at the day of the week people get the loan 
+
+# In[10]:
+
+
+df['dayofweek'] = df['effective_date'].dt.dayofweek
+bins = np.linspace(df.dayofweek.min(), df.dayofweek.max(), 10)
+g = sns.FacetGrid(df, col="Gender", hue="loan_status", palette="Set1", col_wrap=2)
+g.map(plt.hist, 'dayofweek', bins=bins, ec="k")
+g.axes[-1].legend()
+plt.show()
+
+
+# We see that people who get the loan at the end of the week dont pay it off, so lets use Feature binarization to set a threshold values less then day 4 
+
+# In[11]:
+
+
+df['effective_date'] = pd.to_datetime(df['effective_date'])
+
+df['weekend'] = df['dayofweek'].apply(lambda x: 1 if (x>3)  else 0)
+df.head()
+
+
+# ## Convert Categorical features to numerical values
+
+# Lets look at gender:
+
+# In[12]:
+
+
+df.groupby(['Gender'])['loan_status'].value_counts(normalize=True)
+
+
+# 86 % of female pay there loans while only 73 % of males pay there loan
+# 
+
+# Lets convert male to 0 and female to 1:
+# 
+
+# In[13]:
+
+
+df['Gender'].replace(('male','female'),(1,0),inplace=True)
+df.head()
+
+
+# ## One Hot Encoding  
+# #### How about education?
+
+# In[14]:
+
+
+df.groupby(['education'])['loan_status'].value_counts(normalize=True)
+
+
+# #### Feature befor One Hot Encoding
+
+# In[15]:
+
+
+df[['Principal','terms','age','Gender','education']].head()
+
+
+# #### Use one hot encoding technique to conver categorical varables to binary variables and append them to the feature Data Frame 
+
+# In[16]:
+
+
+Feature = df[['Principal','terms','age','Gender','weekend']]
+Feature = pd.concat([Feature,pd.get_dummies(df['education'])], axis=1)
+Feature.drop(['Master or Above'], axis = 1,inplace=True)
+Feature.head()
+
+
+# ### Feature selection
+
+# Lets defind feature sets, X:
+
+# In[17]:
+
+
+X = Feature
+X[0:5]
+
+
+# What are our lables?
+
+# In[18]:
+
+
+y = df['loan_status'].values
+y[0:5]
+
+
+# ## Normalize Data 
+
+# Data Standardization give data zero mean and unit variance (technically should be done after train test split )
+
+# In[19]:
+
+
+X= preprocessing.StandardScaler().fit(X).transform(X)
+X[0:5]
+
+
+# # Classification 
+
+# Now, it is your turn, use the training set to build an accurate model. Then use the test set to report the accuracy of the model
+# You should use the following algorithm:
+# - K Nearest Neighbor(KNN)
+# - Decision Tree
+# - Support Vector Machine
+# - Logistic Regression
+# 
+# 
+# 
+# __ Notice:__ 
+# - You can go above and change the pre-processing, feature selection, feature-extraction, and so on, to make a better model.
+# - You should use either scikit-learn, Scipy or Numpy libraries for developing the classification algorithms.
+# - You should include the code of the algorithm in the following cells.
+
+# # K Nearest Neighbor(KNN)
+# Notice: You should find the best k to build the model with the best accuracy.  
+# **warning:** You should not use the __loan_test.csv__ for finding the best k, however, you can split your train_loan.csv into train and test to find the best __k__.
+
+# In[23]:
+
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(X,y,test_size=0.40,random_state=101)
+
+
+# In[24]:
+
+
+error_rate=[]
+for i in range(1,40):
+    knn = KNeighborsClassifier(n_neighbors=i)
+    knn.fit(x_train,y_train)
+    pred_i = knn.predict(x_test)
+    error_rate.append(np.mean(pred_i != y_test))
+
+
+# In[25]:
+
+
+plt.figure(figsize=(10,6))
+plt.plot(range(1,40),error_rate,color='blue', linestyle='dashed', marker='o',
+         markerfacecolor='red', markersize=10)
+plt.title('Error Rate vs. K Value')
+plt.xlabel('K')
+plt.ylabel('Error Rate')
+
+
+# In[26]:
+
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=7)
+knn.fit(X,y)
+
+
+# In[ ]:
+
+
+
+
+
+# # Decision Tree
+
+# In[27]:
+
+
+from sklearn.tree import DecisionTreeClassifier
+
+
+# In[28]:
+
+
+dtree = DecisionTreeClassifier()
+dtree.fit(X,y)
+
+
+# In[ ]:
+
+
+
+
+
+# # Support Vector Machine
+
+# In[29]:
+
+
+from sklearn.svm import SVC
+
+
+# In[30]:
+
+
+model=SVC()
+model.fit(X,y)
+
+
+# # Logistic Regression
+
+# In[31]:
+
+
+from sklearn.linear_model import LogisticRegression
+
+
+# In[32]:
+
+
+logmodel = LogisticRegression()
+logmodel.fit(X,y)
+
+
+# # Model Evaluation using Test set
+
+# In[33]:
+
+
+from sklearn.metrics import f1_score
+from sklearn.metrics import log_loss
+from sklearn.metrics import classification_report
+
+
+# First, download and load the test set:
+
+# In[34]:
+
+
+get_ipython().system('wget -O loan_test.csv https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/loan_test.csv')
+
+
+# ### Load Test set for evaluation 
+
+# In[35]:
+
+
+test_df = pd.read_csv('loan_test.csv')
+test_df.head()
+
+
+# In[36]:
+
+
+test_df.shape#X1=test_df[['Principal','terms','age','Gender','weekend']]
+
+
+# In[37]:
+
+
+
+test_df['effective_date'] = pd.to_datetime(test_df['effective_date'])
+test_df['dayofweek'] = df['effective_date'].dt.dayofweek
+test_df['weekend'] = test_df['dayofweek'].apply(lambda x: 1 if (x>3)  else 0)
+df.head()
+
+
+# In[46]:
+
+
+test_df.Gender= test_df.Gender.astype(str)
+test_df['Gender'].replace(('male','female'),(1,0),inplace=True)
+Feature1 = test_df[['Principal','terms','age','Gender','weekend']]
+Feature1 = pd.concat([Feature1,pd.get_dummies(test_df['education'])], axis=1)
+Feature1.drop(['Master or Above'], axis = 1,inplace=True)
+X1=Feature1
+X1= preprocessing.StandardScaler().fit(X1).transform(X1)
+y1=test_df['loan_status']
+
+
+# In[47]:
+
+
+pred1=knn.predict(X1)
+
+
+# In[53]:
+
+
+print(classification_report(y1,pred1))
+
+
+# In[52]:
+
+
+pred2=dtree.predict(X1)
+print(classification_report(y1,pred2))
+
+
+# In[50]:
+
+
+pred3=model.predict(X1)
+print(classification_report(y1,pred3))
+
+
+# In[51]:
+
+
+pred4=logmodel.predict(X1)
+print(classification_report(y1,pred4))
+
+
+# # Report
+# You should be able to report the accuracy of the built model using different evaluation metrics:
+
+# | Algorithm          | Jaccard | F1-score | LogLoss |
+# |--------------------|---------|----------|---------|
+# | KNN                | ?       | ?        | NA      |
+# | Decision Tree      | ?       | ?        | NA      |
+# | SVM                | ?       | ?        | NA      |
+# | LogisticRegression | ?       | ?        | ?       |
